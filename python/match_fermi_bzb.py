724734791f3cab70abb6435841cdc9d88895e7a2
KarlenS <shkarlen@gmail.com>
2016-03-08 16:52:02 -0600

    implemented object name matching through NED queries, fixed bugs

--- python/match_fermi_bzb.py
+++ python/match_fermi_bzb.py
@@ -1,15 +1,19 @@
+import sys
 import argparse
 import numpy as np
 from numpy.lib import recfunctions
+
+from itertools import izip as zip, count
+
 from astropy import units as u
-import astropy.io.fits as pf
 from astropy.coordinates import ICRS
+from astropy.table import join, Table
 from scipy.stats.stats import pearsonr
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-from astroquery.simbad import Simbad
-from astroquery.ned import Ned
+#from astroquery.simbad import Simbad
+#from astroquery.ned import Ned
 
 import catalog_utils as utils
 
@@ -23,16 +27,19 @@ class CatalogMatcher(object):
         matches 3FGL with Kyle's catalog;
         returns catalog index of Fermi source matched with Kyle's
         '''
+        #fermi_tab = Table.read(fermi,format='fits')
     
         #first check if the fermi name is 
     
         name_fermi = fermi['ASSOC1']
         name_fermi2 = fermi['ASSOC2']
         name_bzb = bzb['bname']
+
+
         #print name_bzb[1]
         #ned_names = Ned.query_refcode("2010AJ....139..390P")#plotkin
         #ned_names = Ned.query_refcode("2009MNRAS.397.1713C")#Chen
-        
+        ''' 
         ned_names = {}
         for name in name_bzb:
             try:
@@ -40,17 +47,15 @@ class CatalogMatcher(object):
                 ned_names[name] = ned_query["Object Name"][0]
                 #ned_names.append(ned_query["Object Name"])
             except:
-                ned_names[name] = "NO NED"
-                
+                continue
+                #ned_names[name] = "NO NED"
+        '''      
                 
         #wt = ned_names['Object Name']
         #wt = ned_names['Object Name', 'RA(deg)', 'DEC(deg)','Redshift']
         #wt.write("plotkin_objnames.csv")
         #print ned_names
-    
-    
-        print "number of objects:", len(name_fermi)
-    
+        ''' 
         ned_names_fermi = {}
         #simbad_names_fermi = [Simbad.query_objectids(name) for name in name_fermi]
         for name in name_fermi:
@@ -60,38 +65,71 @@ class CatalogMatcher(object):
             except:
                 continue
                 #ned_names_fermi[name] = "NO NED"
-    
+        '''
         #print ned_names_fermi
         #print simbad_fermi
         #theta = Simbad.query_object(name1[0])
         #simbad_assoc = simbad_names["ID"]
+
         ra_fermi = fermi['RAJ2000']
         dec_fermi = fermi['DEJ2000']
         ra_bzb = bzb['ra']
         dec_bzb = bzb['dec']
+        
+        ned_names_bzb = bzb['nedname']
+        ned_names_fermi = fermi['nedname']
+        #print ned_names_bzb
+        #print ned_names_fermi
+
+
+
+        ned_name_idx = []
+        for name in ned_names_bzb:
+            if name == '---bzcat---':
+                continue
+                #ned_name_idx.append(None)
+            else:
+                try:
+                    ind = [i for i, j in zip(count(), ned_names_fermi) if j == name]
+                    ned_name_idx.append(ind[0])
+                except IndexError:
+                    continue
+                    #ned_name_idx.append(None)
+
+                #    ned_name_idx.append(ned_names_fermi.index("---fermi---"))
+                    #ned_name_idx.append(ned_names_fermi.index(name))
+                #except:
+                #    ned_name_idx.append(None)
+
+        print ned_name_idx
     
         #does the coordinate matching
         c_fermi = ICRS(ra_fermi, dec_fermi, unit=(u.degree,u.degree))
         c_bzb = ICRS(ra_bzb, dec_bzb, unit=(u.degree,u.degree))
     
         idx, d2d, d3d = c_bzb.match_to_catalog_sky(c_fermi)
+        print len(idx),len(ned_names_bzb)
     
         matches=c_fermi[idx]
     
         c3fgl_name = 0
         cfermi_ned = 0
-        for name,ned in ned_names.iteritems():
-            if ned in name_fermi or ned in name_fermi2:
+        
+        #blah = join(bzb,fermi_tab,join_type='left')
+        ''' 
+        for name in ned_names_bzb:
+            if name in name_fermi or name in name_fermi2:
                 c3fgl_name+=1
-                print "found bzb match to fermi_assoc_field:", ned
-            elif ned in ned_names_fermi.values():
+                print "found bzb match to fermi_assoc_field:", name
+            elif name in ned_names_fermi:
                 cfermi_ned+=1
-                print "found bzb match to fermi-ned query:", ned
+                print "found bzb match to fermi-ned query:", name
             else:
                 print "no match found"
-    
+        print "Total number of bzb objects:", len(ned_names_bzb)
         print "Matched %d names using 3fgl and %d with NED queries." %(c3fgl_name, cfermi_ned)
-    
+        '''
+        
         dra = (matches.ra - c_bzb.ra).arcmin
         ddec = (matches.dec - c_bzb.dec).arcmin
         ddist = (dra*dra + ddec*ddec)**0.5
@@ -102,6 +140,12 @@ class CatalogMatcher(object):
     
         distmask =  [1 if d[0] < d[1]*60. else 0 for d in zip(ddist,smaja95[idx])]
         match_idx = np.where(distmask)
+
+
+        #index_dict = dict((value, ind) for ind, value in enumerate(idx))
+        #matched_names_idx = [index_dict[name] for name in ned_name_idx]
+        #print len(matched_names_idx)
+
     
         #can keep this the way we're doing but put in a flag for match confidence
         return [ fermi[idx][match_idx], bzb[match_idx], ddist]
@@ -130,7 +174,7 @@ class CatalogMatcher(object):
         bzb = recfunctions.append_fields(bzb, 'class', class1, usemask=False)
         bzb = recfunctions.append_fields(bzb, 'sig', sig, usemask=False)
     
-        np.savetxt(outname, bzb, delimiter=",",fmt="%s", header='bname,btype,ra,dec,catalogue,nt,nb,z,fieldsize,countingmag,bgb,bgb_err,ferminame,fermidist,fermivar,index,class,sig')
+        np.savetxt(outname, bzb, delimiter=",",fmt="%s",comments='', header='bname,btype,ra,dec,catalogue,nt,nb,z,fieldsize,countingmag,bgb,bgb_err,ferminame,fermidist,fermivar,index,class,sig')
     
     def plotParamRelations(bzb,fermi):
     
@@ -168,16 +212,33 @@ class CatalogMatcher(object):
 def main():
 
     parser = argparse.ArgumentParser(description="Code that matches Kyle's bzb catalog with the Fermi 3FGL catalog.")
-    parser.add_argument('--write','-w',default='False', action='store_true')
-    parser.add_argument('--plot','-p',default='False', action='store_true')
+    parser.add_argument('--write','-w',action='store_true',help='Enables writing out of the matched catalog.')
+    parser.add_argument('--plot','-p',action='store_true',help='Enables displaying plots.')
+    parser.add_argument('--supplement','-s',action='store_true',help='Supplementing catalogs with NED names and exits.')
     args = parser.parse_args()
     #for the first pass at this add a column of alternative names in the Fermi-LAT catalog
 
+    if args.supplement:
+        print "--- supplement option enabled ---"
+        utils.supplementBZBWithNED('../data/blazars_bgb.csv')
+        utils.supplementFermiWithNED('../data/gll_psc_v16.fit')
+        sys.exit("Added a column with NED names to 3FGL and bzcat catalog files. Exiting...")
+    
     #reading in catalogs
-    fermi_dat = utils.readFermi('../data/gll_psc_v16.fit')
-    bzb_dat = utils.readBZB('../data/blazars_bgb.csv')
+    try:
+        #fermi_dat = Table.read('../data/gll_psc_v16_sup.fit',format='fits')
+        fermi_dat = utils.readFermi('../data/gll_psc_v16_sup.fit')
+        bzb_dat = utils.readBZB('../data/blazars_bgb_sup.csv')
+    except:
+        sys.exit("ERROR: Run with the supplement option first:  'python match_fermi_bzb.py --supplement'")
+    #fermi_dat = utils.readFermi('../data/gll_psc_v16.fit')
+    #bzb_dat = utils.readBZB('../data/blazars_bgb.csv')
 
     matcher = CatalogMatcher()
+    
+    #tbl = utils.reformatFermiFits(fermi_dat)
+    #print Table.read(tbl)
+    #sys.exit()
 
     #matching and reading of results
     match_results = matcher.matchCats(bzb_dat,fermi_dat)
@@ -186,7 +247,7 @@ def main():
     match_dist = match_results[2] #in arcsec
 
     #write out matched catalog
-    if (args.write):
+    if args.write == True:
         matcher.writeMatchedCat(fermi_matched,bzb_matched,match_dist)
     
     #fermi
@@ -223,7 +284,7 @@ def main():
     z_cut = np.where(z[temp_idx] > 0.15) #cut on redshift
     sig_cut = np.where(sig[temp_idx][z_cut] > 10.) #cut on fermi significance
 
-    if(args.plot):
+    if args.plot == True:
         matcher.plotParamRelations(bzb_matched[temp_idx][z_cut][sig_cut], fermi_matched[temp_idx][z_cut][sig_cut])
     
     print 'num of obj less than 0 clustering:',len(np.where(bgb[temp_idx][z_cut] > 0)[0])
